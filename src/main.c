5e13c6a7444a928489e9dd85c1360ea23f7baf60
Chris Cormack <chris@bigballofwax.co.nz>
2015-07-17 13:27:35 +1200

    Starting on fetching status from jenkins

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <pebble.h>
-
+  
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+  
 static Window *s_main_window;  
 
 static TextLayer *s_time_layer;
@@ -33,6 +36,22 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
    update_time();
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 
@@ -93,6 +112,14 @@ static void init() {
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
   // Create main Window element and assign to pointer
   s_main_window = window_create();
 
