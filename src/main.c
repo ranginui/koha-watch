b4665f5465cc80e38197a2e710c8af747ce10369
Chris Cormack <chris@bigballofwax.co.nz>
2015-07-23 08:23:06 +1200

    Updated version to fix clock alignment

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 55, 144, 50));
+  s_time_layer = text_layer_create(GRect(2, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
